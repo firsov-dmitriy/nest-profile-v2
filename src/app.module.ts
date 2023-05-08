457182ee639bb6ed98d30173c6f3b247a60c6011
firsov.d <xxlaimxx1999@gmail.com>
2023-05-08 11:24:17 +0300

    fix: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { configuration } from '../config/configuration';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({
-      envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
+      envFilePath: `.env`,
       isGlobal: true,
-      load: [configuration],
     }),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.USER_DB_NAME}:${process.env.USER_DB_PASSWORD}@nest.9tbh0dx.mongodb.net/?retryWrites=true&w=majority`,
